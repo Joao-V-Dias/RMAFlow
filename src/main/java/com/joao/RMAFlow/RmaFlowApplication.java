cbb8b0396d47228b32328ef5e1d95e64bb5a8c2d
Joao Dias <joaov.dias08@gmail.com>
2026-08-30 18:47:32 -0300

    Initialize Spring Boot Maven project

---
+++ src/main/java/com/joao/RMAFlow/RmaFlowApplication.java
@@ -0,0 +1,13 @@
+package com.joao.RMAFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmaFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmaFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joao/RMAFlow/RmaFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.RMAFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmaFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
