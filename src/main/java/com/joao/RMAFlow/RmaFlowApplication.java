305b3bfa68b1a3c5ebc56d4ad142f44874f22d3e
Joao Dias <100037973+Joao-V-Dias@users.noreply.github.com>
2026-08-30 21:53:47 +0000

    Create dic

--- src/main/java/com/joao/RMAFlow/RmaFlowApplication.java
+++ src/main/java/com/joao/RMAFlow/RmaFlowApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RmaFlowApplication {
+public class RmaFlowApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RmaFlowApplication.class, args);
